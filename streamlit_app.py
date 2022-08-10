1e5be5199562ebc695044f8c8cced9afa83911fe
Shariq Ahmed <110985759+shariqahm@users.noreply.github.com>
2022-08-10 18:46:01 +0500

    Update streamlit_app.py
    Fixed the wrong emojis & added 3rd menu item

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,8 @@ import streamlit
 
 streamlit.title("My Parent's New Healthy Diner")
 
-streamlit.header("🥣 Breakfast Menu")
-streamlit.text("🥗 Omega 3 & Blueberry Oatmeal")
-streamlit.text("🐔 Kale, Spinach & Rocket Smoothie")
-streamlit.text("🥑🍞 Hard-Boiled Free Range Egg")
+streamlit.header("Breakfast Menu")
+streamlit.text("🥣 Omega 3 & Blueberry Oatmeal")
+streamlit.text("🥗 Kale, Spinach & Rocket Smoothie")
+streamlit.text("🐔 Hard-Boiled Free Range Egg")
+streamlit.text("🥑🍞 Avocado Toast")
