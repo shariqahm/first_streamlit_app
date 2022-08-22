0cee41e979471207fe98143299f03a98b72e0f65
Shariq Ahmed <110985759+shariqahm@users.noreply.github.com>
2022-08-22 17:43:28 +0500

    Update streamlit_app.py
    function get_fruityvice_data added

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,6 +24,14 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
+# Create a repeatable code block (function)
+def get_fruityvice_data(this_fruit_choice):
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+    # streamlit.text(fruityvice_response.json()) # just writes the data to the screen
+    # take the json version of the response & normalize it!
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    return fruityvice_normalized
+
 # New section to display Fruityvice API Response
 streamlit.header("Fruityvice Fruit Advice!")
 try:
@@ -33,12 +41,9 @@ try:
 # streamlit.write('The user entered ', fruit_choice)
 
     else:
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-        # streamlit.text(fruityvice_response.json()) # just writes the data to the screen
-        # take the json version of the response & normalize it!
-        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+        back_from_function = get_fruityvice_data(fruit_choice)
         # output the result in table format
-        streamlit.dataframe(fruityvice_normalized)
+        streamlit.dataframe(back_from_function)
 
 except URLError as e:
     streamlit.error()
