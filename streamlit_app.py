75cb8702295d46a2045c5ea7d57e50565d5f728c
Shariq Ahmed <110985759+shariqahm@users.noreply.github.com>
2022-08-22 15:51:13 +0500

    Update streamlit_app.py
    added try-except for error handling

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,16 +26,22 @@ streamlit.dataframe(fruits_to_show)
 
 # New section to display Fruityvice API Response
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+try:
+    fruit_choice = streamlit.text_input('What fruit would you like information about?')
+    if not fruit_choice:
+        streamlit.error("Please select a fruit to get information.")
+# streamlit.write('The user entered ', fruit_choice)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-# streamlit.text(fruityvice_response.json()) # just writes the data to the screen
+else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    # streamlit.text(fruityvice_response.json()) # just writes the data to the screen
+    # take the json version of the response & normalize it!
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    # output the result in table format
+    streamlit.dataframe(fruityvice_normalized)
 
-# take the json version of the response & normalize it!
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# output the result in table format
-streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+    streamlit.error()
 
 # streamlit.stop()
 
@@ -45,7 +51,6 @@ my_cur.execute("SELECT * FROM fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit list contains:")
 streamlit.dataframe(my_data_rows)
-
 
 # Allow the end user to add a fruit to the list
 add_my_fruit = streamlit.text_input('What fruit would you like information about?','jackfruit')
