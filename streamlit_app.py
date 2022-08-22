9639beef61cceab8eb13f8ea3bccad1519a1f658
Shariq Ahmed <110985759+shariqahm@users.noreply.github.com>
2022-08-22 14:46:11 +0500

    Update streamlit_app.py
    removed import snowflake.connector

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,4 +36,4 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # output the result in table format
 streamlit.dataframe(fruityvice_normalized)
 
-import snowflake.connector
+
